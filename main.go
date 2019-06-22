08681ddef7e91ad76c8a5695ad636dd06f782abf
Michael Bruce <me@michaelbruce.co>
2019-06-22 10:54:04 +0100

    proof of concept - printing out simple query

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	consumerKey       = getenv("TWITTER_CONSUMER_KEY")
+	consumerSecret    = getenv("TWITTER_CONSUMER_SECRET")
+	accessToken       = getenv("TWITTER_ACCESS_TOKEN")
+	accessTokenSecret = getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+func getenv(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		panic("missing required environment variable " + name)
+	}
+	return v
+}
+
+func main() {
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
+
+	log := &logger{logrus.New()}
+	api.SetLogger(log)
+
+	searchResult, _ := api.GetSearch("golang", nil)
+	for _, tweet := range searchResult.Statuses {
+		fmt.Println(tweet.Text)
+	}
+}
+
+type logger struct {
+	*logrus.Logger
+}
+
+func (log *logger) Critical(args ...interface{})                 { log.Error(args...) }
+func (log *logger) Criticalf(format string, args ...interface{}) { log.Errorf(format, args...) }
+func (log *logger) Notice(args ...interface{})                   { log.Info(args...) }
+func (log *logger) Noticef(format string, args ...interface{})   { log.Infof(format, args...) }
